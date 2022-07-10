2f291066405d95b4d04b7b3183455fb571802f88
Konstantin aka costas <ks.costas@gmail.com>
2022-07-10 12:28:56 +0300

    Update encryption.php

--- encryption.php
+++ encryption.php
@@ -1,26 +1,27 @@
 <?php
 
-define('AES_METHOD', 'aes-256-cbc');
+const AES_METHOD = 'aes-256-cbc';
 
-$password = 'lbwyBzfgzUIvXZFShJuikaWvLJhIVq36';
-
-function encrypt($message, $password)
+function encrypt($message, $key)
 {
     if (OPENSSL_VERSION_NUMBER <= 268443727) {
         throw new RuntimeException('OpenSSL Version too old, vulnerability to Heartbleed');
     }
-    
-    $iv_size        = openssl_cipher_iv_length(AES_METHOD);
-    $iv             = openssl_random_pseudo_bytes($iv_size);
-    $ciphertext     = openssl_encrypt($message, AES_METHOD, $password, OPENSSL_RAW_DATA, $iv);
-    $ciphertext_hex = bin2hex($ciphertext);
-    $iv_hex         = bin2hex($iv);
-    return "$iv_hex:$ciphertext_hex";
+
+    $ivSize = openssl_cipher_iv_length(AES_METHOD);
+    $iv = openssl_random_pseudo_bytes($ivSize);
+    $cipherText = openssl_encrypt($message, AES_METHOD, $key, OPENSSL_RAW_DATA, $iv);
+    $cipherTextHex = bin2hex($cipherText);
+    $ivHex = bin2hex($iv);
+
+    return "$ivHex:$cipherTextHex";
 }
-function decrypt($ciphered, $password) {
-    $iv_size    = openssl_cipher_iv_length(AES_METHOD);
-    $data       = explode(":", $ciphered);
-    $iv         = hex2bin($data[0]);
-    $ciphertext = hex2bin($data[1]);
-    return openssl_decrypt($ciphertext, AES_METHOD, $password, OPENSSL_RAW_DATA, $iv);
+
+function decrypt($ciphered, $key)
+{
+    $data = explode(":", $ciphered);
+    $iv = hex2bin($data[0]);
+    $cipherText = hex2bin($data[1]);
+
+    return openssl_decrypt($cipherText, AES_METHOD, $key, OPENSSL_RAW_DATA, $iv);
 }
